d1c42e759972616a9d343afad0fc5e236c3344d4
Guillaume Ecollan <guillaume@ecollan.fr>
2019-02-25 18:08:37 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+  devise_for :users
   resources :projects do
     resources :sessions
   end
-=======
-  devise_for :users
-  resources :projects
->>>>>>> 14543e748f7467994698fec880a37bf8fa527bd2
 end

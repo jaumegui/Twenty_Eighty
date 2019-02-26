8ce48874cee3dda46eb7cc370f107b2ecd895ea2
Guillaume Ecollan <guillaume@ecollan.fr>
2019-02-26 16:41:28 +0100

    add expertise and chapter

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,3 @@
 class Content < ApplicationRecord
+	validates :chapter, inclusion: { in: %w[ Communication Developpement-Personnel Innovation Management Négociation Stratégie Leadership ] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -90,7 +90,8 @@ ActiveRecord::Schema.define(version: 2019_02_25_155208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.boolean "admin", default: false
+    t.string "access_level", default: "sevener"
+    t.string "expertise"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

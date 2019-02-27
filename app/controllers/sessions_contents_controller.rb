bf53fed1c79c1c3ce4fb442afe0f624b6bd22f56
Brice Chapuis <brice.chapuis@gmail.com>
2019-02-27 15:04:56 +0100

    test

--- app/controllers/sessions_contents_controller.rb
+++ app/controllers/sessions_contents_controller.rb
@@ -1,20 +1,37 @@
 class SessionsContentsController < ApplicationController
-  def new
-    @session = Session.find(params[:session_id])
-    @contents = Content.all
-    @sessionscontent = SessionsContent.new
+  def show
+    @sessionscontent = SessionsContent.find(params[:id])
   end
 
   def create
-    @sessionscontent = SessionsContent.new
+    @sessionscontent = SessionsContent.create
     @session = Session.find(params[:session_id])
     @sessionscontent.session = @session
     @sessionscontent.content = Content.find(params[:content_id])
+    @sessionscontent.title = @sessionscontent.content.title
+    @sessionscontent.description = @sessionscontent.content.description
+    @sessionscontent.chapter = @sessionscontent.content.chapter
+    @sessionscontent.duration = @sessionscontent.content.duration
+    @sessionscontent.format = @sessionscontent.content.format
+    @sessionscontent.order = @sessionscontent.id
+    redirect_to project_session_path(@session.project, @session)
+  end
+
+  def edit
+    @sessionscontent = SessionsContent.find(params[:id])
+  end
+
+  def update
+    @sessionscontent = SessionsContent.find(params[:id])
+    @sessionscontent.update(sessionscontent_params)
     if @sessionscontent.save
-      redirect_to project_session_path(@session.project, @session)
+      redirect_to project_session_path(@sessionscontent.session.project, @sessionscontent.session)
     else
-      render :new
+      render :edit
     end
+  end
+
+  def destroy
   end
 
   private

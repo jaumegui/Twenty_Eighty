8f31585cce97d7644be8f285daeff3d485534810
Brice Chapuis <brice.chapuis@gmail.com>
2019-02-27 10:55:40 +0100

    intelcontent controller done

--- app/controllers/intelligence_contents_controller.rb
+++ app/controllers/intelligence_contents_controller.rb
@@ -2,13 +2,14 @@ class IntelligenceContentsController < ApplicationController
   def new
     @content = Content.find(params[:content_id])
     @intelcontent = IntelligenceContent.new
+    @intelligences = Intelligence.all
   end
 
   def create
     @content = Content.find(params[:content_id])
     @intelcontent = IntelligenceContent.new
-    @intelcontent.content = @session
-    @intelcontent.intelligence = Intelligence.find(params[:intelligence_content][:content].to_i)
+    @intelcontent.content = @content
+    @intelcontent.intelligence = Intelligence.find(params[:intelligence_content][:intelligence].to_i)
     if @intelcontent.save
       redirect_to content_path(@content)
     else
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ApplicationRecord
-  has_many :sessions_contents, dependant: :destroy
+  has_many :sessions_contents, dependent: :destroy
   has_many :sessions, through: :sessions_contents
   has_many :intelligence_contents, dependent: :destroy
   has_many :intelligences, through: :intelligence_contents

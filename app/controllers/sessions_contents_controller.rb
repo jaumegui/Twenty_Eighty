b1353988ef0850c74ad886fd8c8ae33de07f805c
Brice Chapuis <brice.chapuis@gmail.com>
2019-02-27 15:16:21 +0100

    update

--- app/controllers/sessions_contents_controller.rb
+++ app/controllers/sessions_contents_controller.rb
@@ -4,7 +4,7 @@ class SessionsContentsController < ApplicationController
   end
 
   def create
-    @sessionscontent = SessionsContent.create
+    @sessionscontent = SessionsContent.new
     @session = Session.find(params[:session_id])
     @sessionscontent.session = @session
     @sessionscontent.content = Content.find(params[:content_id])
@@ -14,6 +14,7 @@ class SessionsContentsController < ApplicationController
     @sessionscontent.duration = @sessionscontent.content.duration
     @sessionscontent.format = @sessionscontent.content.format
     @sessionscontent.order = @sessionscontent.id
+    @sessionscontent.save
     redirect_to project_session_path(@session.project, @session)
   end
 
@@ -32,6 +33,8 @@ class SessionsContentsController < ApplicationController
   end
 
   def destroy
+    @sessionscontent = SessionsContent.find(params[:id])
+    @sessionscontent.destroy
   end
 
   private

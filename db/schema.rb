081aa5abce47fc26c36423c94b5d4cecbc6ae630
Brice Chapuis <brice.chapuis@gmail.com>
2019-03-01 10:48:16 +0100

    fixed project creation with client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_153214) do
+ActiveRecord::Schema.define(version: 2019_03_01_094419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 2019_02_28_153214) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "participant_number", default: 0
+    t.bigint "client_id"
+    t.index ["client_id"], name: "index_projects_on_client_id"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
@@ -110,6 +112,7 @@ ActiveRecord::Schema.define(version: 2019_02_28_153214) do
 
   add_foreign_key "comments", "sessions"
   add_foreign_key "comments", "users"
+  add_foreign_key "projects", "clients"
   add_foreign_key "projects", "users"
   add_foreign_key "sessions", "projects"
   add_foreign_key "user_projects", "projects"

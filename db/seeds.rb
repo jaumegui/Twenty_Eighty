d24a27c9507d4ab25f4a8126c398830917183d09
Benoit Roualec <benoit.roualec@gmail.com>
2019-03-01 15:12:31 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 puts 'Cleaning database...'
-Content.destroy_all
-Comment.destroy_all
-User.destroy_all
 Client.destroy_all
+Content.destroy_all
+User.destroy_all
+Comment.destroy_all
+Project.destroy_all
+Session.destroy_all
+Mod.destroy_all
+User_projects.destroy_all
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).

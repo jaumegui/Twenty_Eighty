9642a23059251082dffce022db3c8521c7100abe
Brice Chapuis <brice.chapuis@gmail.com>
2019-03-01 16:18:21 +0100

    miscellanous fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
   has_many :user_project_as_sevener, source: :user_projects
   has_many :user_project_as_admin, through: :projects, source: :user_projects
   has_many :comments
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   validates :access_level, inclusion: { in: %w(admin project_manager sevener)}
   validates :expertise, inclusion: { in: EXPERTISES }
 end

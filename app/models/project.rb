961db0310a6a31999724120605a0efaa1fc0813b
Brice Chapuis <brice.chapuis@gmail.com>
2019-03-04 15:45:23 +0100

    Access authorization done

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ApplicationRecord
   belongs_to :user
   belongs_to :client
   has_many :user_projects, dependent: :destroy
+  has_many :users, through: :user_projects
   validates :title, presence: true
   validates :participant_number, presence: true
 end

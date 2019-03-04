ce8834c6f7cf667157170005db818a1fad936b67
Benoit Roualec <benoit.roualec@gmail.com>
2019-03-04 16:38:32 +0100

    Added margin to user-manager button

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,4 +3,10 @@ class Session < ApplicationRecord
   has_many :mods, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :mods, -> { order(position: :asc) }, dependent: :destroy
+
+  def date_include_in_project_dates?
+    if date < project.start_date || date > project.end_date
+      errors.add(:date, 'Must be included in project dates')
+    end
+  end
 end
